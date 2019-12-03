5deb94938740bff3b158912c2939f640a9a036e5
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-12-03 16:42:03 +0100

    adding standard test script

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PWD=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+go get -u github.com/lib/pq
+go test -v -cover
+RESULT=$?
+cd $PWD
+exit $RESULT
+
